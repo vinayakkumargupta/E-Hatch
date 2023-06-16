d22e08854c76b409798d648c2797590894d7f73e
Vinayak Dashboard <vinayakkr9304@gmail.com>
2023-06-17 02:04:11 +0530

    LoginPage

---
+++ app/src/main/java/com/example/e_hatch/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.e_hatch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_hatch.Auth.LoginMainActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
